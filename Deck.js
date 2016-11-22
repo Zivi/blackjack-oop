acda923e7ffb45b1c2dbb2b5bf05900fbef4af15
Zivi Weinstock <zivi.weinstock@gmail.com>
2016-11-21 21:04:35 -0800

    Deck unit tests.

--- Deck.js
+++ Deck.js
@@ -1,5 +1,6 @@
 var Card = require('./Card');
 var suits = ['spades', 'clubs', 'hearts', 'diamonds'];
+var _ = require('lodash');
 function Deck() {
   this.cards = [];
   for (var i = 0; i < 52; i += 1) {
@@ -7,5 +8,9 @@ function Deck() {
     var j = Math.floor(i / 13);
     this.cards.push(new Card(value, suits[j]));
   }
+}
+
+Deck.prototype.shuffle = function() {
+  this.cards = _.shuffle(this.cards);
 }
 module.exports = Deck;
---
+++ test/Deck.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var Deck = require('../Deck');
+describe('Deck', function() {
+  describe('#constructor', function() {
+    it('should have a length of 52', function() {
+      var deck = new Deck();
+      assert.equal(deck.cards.length, 52);
+    });
+    it('should have a first card with value 1 and suit spades', function() {
+      var deck = new Deck();
+      assert.deepEqual(deck.cards[0], {value: 1, suit: 'spades' });
+    });
+    it('should have a last card with value 10 and suit diamonds', function() {
+      var deck = new Deck();
+      assert.deepEqual(deck.cards[51], {value: 10, suit: 'diamonds' });
+    });
+  });
+  describe('#shuffle', function() {
+    it('should have the cards in different positions', function() {
+      var deck = new Deck();
+      var cardsBefore = deck.cards;
+      deck.shuffle();
+      assert.notDeepEqual(cardsBefore, deck.cards);
+    })
+  })
+});
